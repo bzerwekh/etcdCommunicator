4b5db34d10762bb64b1529a1042ac77a59a4bee5
bzerwekh <brandon.zerwekh@gmail.com>
2014-08-04 00:11:12 -0500

    Initial checkin
    start files

---
+++ etcd.DataContracts/Key.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace etcd.DataContracts
+{
+    [DataContract]
+    public class Key
+    {
+        [DataMember]
+        public string Action { get; set; }
+
+        [DataMember]
+        public Node Node { get; set; }
+    }
+}
---
+++ etcd.DataContracts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace etcd.DataContracts
+{
+    [DataContract]
+    public class Node
+    {
+        [DataMember(Order = 1)]
+        public int CreatedIndex { get; set; }
+
+        [DataMember(Order = 2)]
+        public string Key { get; set; }
+
+        [DataMember(Order = 3)]
+        public int ModifiedIndex { get; set; }
+
+        [DataMember(Order = 4)]
+        public string Value { get; set; }
+    }
+}
